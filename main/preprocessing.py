91d54380110b1f29b868bf40be503bb7ea2aa333
Daniele <daniele.davoli@outlook.com>
2019-06-18 19:09:09 +0200

    reorganization of files

--- main/preprocessing.py
+++ main/preprocessing.py
@@ -1,1 +1,75 @@
-jknvIjdsnkvsdjnv
+import nltk
+import csv
+import string
+import re
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import sent_tokenize
+
+'''
+the prepocessing is composed by:
+	- punctuaction elimination
+	- stop words elimination
+	- stemming
+	- digits elimination
+	- extra spaces elimination
+'''
+
+#main function
+def doPreprocessing(tweet_message):
+	print("Original tweet: " + tweet_message)
+
+	tweet_message = removePunct(tweet_message)
+	print("Removed punctuaction: " + str(tweet_message))
+
+	tweet_message = removeStopWords(tweet_message)
+	print("Removed stopwords: " + str(tweet_message))
+
+	tweet_message = doStemming(tweet_message)
+	print("Stemming: " + str(tweet_message))
+
+	#tweet_message = doLemmatization(tweet_message)
+	#print("Lemmatization: " + str(tweet_message))
+
+	tweet_message = removeDigits(tweet_message)
+	print("Removed Digits: " + tweet_message)
+
+	#remove double spaces and unwanted spaces
+	return re.sub(' +',' ',tweet_message).strip()
+
+#divide the string in tokens while eliminating the punctuation
+def removePunct(tweet_message):
+	tokenizer = RegexpTokenizer(r'\w+')
+	return tokenizer.tokenize(tweet_message.lower())
+
+#remove stop word in the tokenized tweet message
+def removeStopWords(tokens):
+	stop_words = set(stopwords.words('italian')) #english
+	filtered_sentence = []
+	for token in tokens:
+		if token not in stop_words:
+			filtered_sentence.append(token)
+	return filtered_sentence
+
+def doStemming(tokens):
+	ita_stemmer = nltk.stem.snowball.ItalianStemmer()
+	for i in range(0, len(tokens):
+		tokens[i] = ita_stemmer.stem(tokens[i])
+	tweet_message = " ".join(str(x) for x in tokens)
+	return tweet_message
+
+def removeDigits(tweet_message):
+	remove_digits = str.maketrans('', '', string.digits)
+	clean_message = tweet_message.translate(remove_digits)
+	return clean_message
+
+
+
+
+
+
+
+
+
